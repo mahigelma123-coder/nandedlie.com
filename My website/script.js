bcaf7644e3e2f4a679cfd17e950eb84788a1e10e
mahigelma123-coder <mahigelma123@gmail.com>
2025-11-01 16:21:43 +0530

    Add files via upload

---
+++ My website/script.js
@@ -0,0 +1,4 @@
+document.getElementById('greetBtn').addEventListener('click', function(){
+  const msg = 'हॅलो! तुम्ही वेबसाइट बनवायला सुरुवात केली आहे 🎉';
+  document.getElementById('greetMsg').textContent = msg;
+});
